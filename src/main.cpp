c47acf2cd47a3b41b8a6221633a771b6653a3b9a
Sulthan Daffa <sdaffa568@gmail.com>
2022-04-05 10:01:21 +0700

    Adding Additional Files

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "include/person.hpp"
 #include "include/mahasiswa.hpp"
@@ -7,6 +8,38 @@
 #include "include/tendik.hpp"
 
 using namespace std;
+
+mahasiswa AddMahasiswa(int id){
+	string nama, nrp, departemen;
+	int dd, mm, yy, tahunMasuk;
+
+	while(1){
+		system("clear");
+		cout << "Masukkan Nama: ";
+		cin >> nama;
+		cout << endl << "Masukkan Tanggal Lahir(dd mm yy): ";
+		cin >> dd >> mm >> yy;
+		cout << endl << "Masukkan Departemen: ";
+		cin >> departemen;
+		cout << endl << "Masukkan Tahun Masuk: ";
+		cin >> tahunMasuk;
+
+		cout << endl << endl;
+		cout << "Nama: " << nama << endl;
+		cout << "Tanggal Lahir: " << dd << "-" << mm << "-" << yy << endl;
+		cout << "Departemen: " << departemen << endl;
+		cout << "Tahun Masuk: " << tahunMasuk << endl;
+		cout << endl << "Apakah Data Yang Dimasukkan Sudah Benar?(Y/N)" << endl;
+
+		char YN;
+		while(YN != 'Y' && YN != 'y' && YN != 'N' && YN != 'n')
+			cin >> YN;
+
+		if(YN == 'Y' || YN == 'y') break;
+	}
+
+	return mahasiswa(to_string(id), nama, dd, mm, yy, nrp, departemen, tahunMasuk);
+}
 
 int main(int argc, char** argv)
 {
@@ -16,7 +49,11 @@ int main(int argc, char** argv)
 
 	int menu_terpilih;
 
+	system("clear");
+
 	while(1) {
+		
+
 		cout << "Selamat datang di Universitas X" << endl << endl;
 		cout << "Data statistik:" << endl;
 		cout << "  1. Jumlah Mahasiswa             : " << recMhs.size() << " mahasiswa" << endl;
@@ -35,19 +72,23 @@ int main(int argc, char** argv)
 
 		switch (menu_terpilih) {
 			case 1:
+				recMhs.push_back(AddMahasiswa(recMhs.size()));
+				system("clear");
+				cout << "Pembuatan Mahasiswa Baru Berhasil" << endl << endl;
 				break;
 			case 2:
 				break;
 			case 3:
-				break;:
+				break;
 			case 4:
-				break;:
+				break;
 			case 5:
-				break;:
+				break;
 			case 6:
-				break;:
+				break;
 		}
 	}
 
 	return 0;
 }
+
