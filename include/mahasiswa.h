63e46b73168b2ccdd5845cbfb1548da4d689953c
Sulthan Daffa <sdaffa568@gmail.com>
2022-04-11 03:35:44 +0700

    remove .o

--- include/mahasiswa.h
+++ include/mahasiswa.h
@@ -46,46 +46,7 @@ enum departements{
 	Statistika_Bisnis,
 };
 
-std::string departementsStr[38] = {
-	"Fisika",
-	"Matematika",
-	"Statistika",
-	"Kimia",
-	"Biologi",
-	"Aktuaria",
-	"Teknik Pekapalan",
-	"Teknik Sistem Perkapalan",
-	"Teknik Kelautan",
-	"Teknik Transportasi Laut",
-	"Teknik Mesin",
-	"Teknik Fisika",
-	"Teknik Sistem dan Industri",
-	"Teknik Material",
-	"Teknik Elektro",
-	"Teknik Biomedik",
-	"Teknik Komputer",
-	"Teknik Informatika",
-	"Sistem Informasi",
-	"Teknologi Informasi",
-	"Teknik Sipil",
-	"Arsitektur",
-	"Teknik Lingkungan",
-	"Perencanaan Wilayah dan Kota",
-	"Teknik Geomatika",
-	"Teknik Geofisika",
-	"Desain Produk Industri",
-	"Desain Interior",
-	"Desain Komunikasi Visual",
-	"Manajemen Bisnis",
-	"Studi Pembangunan",
-	"Manajemen Teknologi",
-	"Teknik Infrastruktur Sipil",
-	"Teknik Mesin Industri",
-	"Teknik Elektro Otomasi",
-	"Teknik Kimia Industri",
-	"Teknik Instrumentasi",
-	"Statistika Bisnis"
-};
+extern std::string departementsStr[38];
 
 class mahasiswa : public person {
 private:
